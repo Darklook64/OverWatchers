b67cad369f22f5ae0e9b6e3cb74ac304e1bd7f8f
ChaoticTale <jrcortest1@gmail.com>
2016-05-31 13:10:41 -0300

    Versión 2

--- bd/Data.php
+++ bd/Data.php
@@ -47,6 +47,40 @@ class Data{
           $this->c->ejecutar($query);
      }
 
+     public function listarUsuario(){
+          $query = "select u.id, u.nombre, u.correo, p.permiso as 'Permiso'
+          from usuario u, permiso p
+          where u.permiso = p.id
+          order by u.nombre asc;";
+
+          $rs = $this->c->ejecutar($query);
+          echo "<h1>Listado de Usuarios</h1>";
+
+          echo "<table border='1'>";
+          echo "<tr>";
+              echo "<th>ID</th>";
+              echo "<th>Nombre</th>";
+              echo "<th>Correo</th>";
+              echo "<th>Permiso</th>";
+              echo "<th>Eliminar</th>";
+          echo "</tr>";
+          while($reg= mysql_fetch_array($rs)){
+            echo "<tr>";
+              echo "<td>$reg[0]</td>";
+              echo "<td>$reg[1]</td>";
+              echo "<td>$reg[2]</td>";
+              echo "<td>$reg[3]</td>";
+              echo "<td><a href='controlador/eliminar.php?id=$reg[0]'>Delete</a></td>";
+            echo "</tr>";
+
+          }
+          echo "</table>";
+     }
+
+
+
+     // Héroes
+
      public function getHeroe(){
           $query = "select nombre from Heroe where id"
 
